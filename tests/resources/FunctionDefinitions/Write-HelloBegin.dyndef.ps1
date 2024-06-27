d16d031fc2748d3813987c6056b90dc3fc846b7a
wethreetrees <tyl0rz@gmail.com>
2024-06-27 00:54:25 -0400

    test: add new test cases to support comment based help generation

--- tests/resources/FunctionDefinitions/Write-HelloBegin.dyndef.ps1
+++ tests/resources/FunctionDefinitions/Write-HelloBegin.dyndef.ps1
@@ -1,3 +1,27 @@
+<#
+.COMPONENT
+    WriteHello
+.DESCRIPTION
+    Writes a greeting to the console.
+.EXAMPLE
+    Write-HelloBegin
+    Hello, World!
+.INPUTS
+    None
+.LINK
+    https://github.com/wethreetrees/Dynamic
+.NOTES
+    This is a test function for the dynamic parameter resolution module.
+.OUTPUTS
+    System.String
+.PARAMETER NAME
+    The name of the person to greet
+.ROLE
+    Write
+.SYNOPSIS
+    Writes a greeting to the console.
+#>
+
 function Write-HelloBegin {
     [CmdletBinding()]
     param (
--- tests/resources/TestFunctions/Send-FoodOrder.ps1
+++ tests/resources/TestFunctions/Send-FoodOrder.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS
+    Sends a food order to the kitchen.
+.DESCRIPTION
+    Sends a food order to the kitchen.
+.EXAMPLE
+    Send-FoodOrder -Type sandwich -Bread wheat
+    Ordering a wheat bread sandwich
+.EXAMPLE
+    Send-FoodOrder -Type pizza -Crust thin
+    Ordering a thin crust pizza
+.PARAMETER Type
+    The type of food to order.
+.PARAMETER Bread
+    The type of bread to use for a sandwich.
+.PARAMETER Crust
+    The type of crust to use for a pizza.
+.OUTPUTS
+    System.String
+.NOTES
+    This is a test function for the dynamic parameter resolution module.
+#>
+
 function Send-FoodOrder {
     [CmdletBinding()]
     param (
